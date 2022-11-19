4da4fa3e75c3616cc3cf23e81ec53e14004d050c
Faye <1337faye@gmail.com>
2022-11-20 07:05:01 +1100

    Fixed menu & Footer

--- menu.js
+++ menu.js
@@ -1,1 +1,27 @@
-const button=document.getElementById("menubutton"),menu=document.getElementById("menu");var open="false";function openmenu(){"false"==open?(button.classList.remove("fa-bars"),button.classList.add("fa-x"),menu.classList.add("translate-x-0"),menu.classList.remove("translate-x-56"),open="true"):(button.classList.add("fa-bars"),button.classList.remove("fa-x"),menu.classList.remove("translate-x-0"),menu.classList.add("translate-x-56"),open="false")}
+const button = document.getElementById("menubutton")
+const menu = document.getElementById("menu")
+var open = "false"
+async function openmenu() {
+    if (open == "false") {
+        menu.classList.remove("hidden")
+        await sleepNow(100)
+        button.classList.remove("fa-bars")
+        button.classList.add("fa-x")
+        menu.classList.add("translate-x-0")
+        menu.classList.remove("translate-x-56")
+
+        open = "true"
+
+    }else{
+
+        button.classList.add("fa-bars")
+        button.classList.remove("fa-x")
+        menu.classList.remove("translate-x-0")
+        menu.classList.add("translate-x-56")
+        open = "false"
+
+        await sleepNow(300)
+        menu.classList.add("hidden")
+        }
+    }
+
