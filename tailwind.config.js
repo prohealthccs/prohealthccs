02817f564fb210496b8706c000b48bc2126b0720
EntityRender <xxayo23xx@gmail.com>
2022-11-21 17:20:40 +1100

    Added apply page & some small design touches

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['index.html', 'world.html','contact.html','services.html/'],
+  content: ['index.html', 'world.html','contact.html','services.html','apply.html'],
   theme: {
     extend: {
       fontFamily: {
